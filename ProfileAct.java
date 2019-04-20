f5e6758ea31c21eb87baabddf4393299d6ee548d
djemoui <34734830+badrdjemoui@users.noreply.github.com>
2019-04-20 16:26:13 +0100

    Create ProfileAct.java

---
+++ ProfileAct.java
@@ -0,0 +1,14 @@
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfileAct extends AppCompatActivity {
+TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        tv = (TextView) findViewById(R.id.tvWelcome);
+        tv.setText("welcome"+MyDb.username);
+    }
+}
